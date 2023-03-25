0ae832f19459ca9a634be7c0d82dcb35ac5b4230
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2023-03-25 20:48:06 +0000

    update oauth to signin with google

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,20 @@
-import { Flex, Button, Image } from "@chakra-ui/react";
+import { Flex, Button, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
-const OAuthButtons: React.FC = () => {
+type OAuthButtonsProps = {};
+
+const OAuthButtons: React.FC<OAuthButtonsProps> = () => {
+  const [SignInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        isLoading={loading}
+        onClick={() => SignInWithGoogle()}
+      >
         <Image
           src="images/googlelogo.png"
           alt="google logo"
@@ -14,6 +24,11 @@ const OAuthButtons: React.FC = () => {
         Continue with Google
       </Button>
       <Button variant="oauth">Some other provider</Button>
+      {error && (
+        <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
+          {error.message}
+        </Text>
+      )}
     </Flex>
   );
 };
