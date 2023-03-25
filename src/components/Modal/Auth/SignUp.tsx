f997e95ca158dcdeb7c6db1d9c065721fa499aeb
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2023-03-25 15:14:40 +0000

    firebase setup

--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -2,105 +2,82 @@ import { authModalState } from "@/atoms/authModalAtom";
 import { Input, Button, Flex, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 const SignUp: React.FC = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
-  const [signUpForm, setSignUpForm] = useState({
+
+  const [form, setForm] = useState({
     email: "",
     password: "",
     confirmPassword: "",
   });
+  const [formError, setFormError] = useState("");
+  const [createUserWithEmailAndPassword, _, loading, authError] =
+    useCreateUserWithEmailAndPassword(auth);
 
-  // Firebase logic
-  const onSubmit = () => {};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (formError) setFormError("");
+    if (!form.email.includes("@")) {
+      return setFormError("Please enter a valid email");
+    }
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // update form state
-    setSignUpForm((prev) => ({
+    if (form.password !== form.confirmPassword) {
+      return setFormError("Passwords do not match");
+    }
+
+    // Valid form inputs
+    createUserWithEmailAndPassword(form.email, form.password);
+  };
+
+  const onChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({
       ...prev,
-      [event.target.name]: event.target.value,
+      [name]: value,
     }));
   };
 
   return (
     <form onSubmit={onSubmit}>
       <Input
-        required
         name="email"
         placeholder="email"
-        type="email"
+        type="text"
         mb={2}
-        onChange={() => {
-          onChange;
-        }}
-        fontSize="10pt"
-        _placeholder={{ color: "gray.500" }}
-        _hover={{
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        _focus={{
-          outline: "none",
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        bg="gray.50"
+        onChange={onChange}
       />
       <Input
-        required
         name="password"
         placeholder="password"
         type="password"
         mb={2}
-        onChange={() => {
-          onChange;
-        }}
-        fontSize="10pt"
-        _placeholder={{ color: "gray.500" }}
-        _hover={{
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        _focus={{
-          outline: "none",
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        bg="gray.50"
+        onChange={onChange}
       />
       <Input
-        required
         name="confirmPassword"
         placeholder="confirm password"
         type="password"
-        mb={2}
-        onChange={() => {
-          onChange;
-        }}
-        fontSize="10pt"
-        _placeholder={{ color: "gray.500" }}
-        _hover={{
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        _focus={{
-          outline: "none",
-          bg: "white",
-          border: "1px solid",
-          borderColor: "blue.500",
-        }}
-        bg="gray.50"
+        onChange={onChange}
       />
-      <Button width="100%" height="36px" type="submit" mt={2} mb={2}>
+      <Text textAlign="center" mt={2} fontSize="10pt" color="red">
+        {formError}
+      </Text>
+      <Button
+        width="100%"
+        height="36px"
+        mb={2}
+        mt={2}
+        type="submit"
+        isLoading={loading}
+      >
         Sign Up
       </Button>
       <Flex fontSize="9pt" justifyContent="center">
-        <Text mr={1}>Already a redditor?</Text>
+        <Text mr={1}>Have an account?</Text>
         <Text
           color="blue.500"
           fontWeight={700}
@@ -108,7 +85,7 @@ const SignUp: React.FC = () => {
           onClick={() => {
             setAuthModalState((prev) => ({
               ...prev,
-              view: "login",
+              view: "signup",
             }));
           }}
         >
