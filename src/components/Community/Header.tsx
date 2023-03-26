40e1bd778438ba33fcc4fd7abc1e0a04dedf6c64
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2023-03-26 19:48:41 +0100

    added join and leave communities

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -2,13 +2,20 @@ import { Community } from "@/atoms/communitiesAtom";
 import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaReddit } from "react-icons/fa";
+import useCommunityData from "../../hooks/useCommunityData";
 
 type HeaderProps = {
   communityData: Community;
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
-  const isJoined = false; //read from community snippet
+  const { communityStateValue, onJoinOrLeaveCommunity, loading } =
+    useCommunityData();
+  // !! converts the value to a boolean
+  const isJoined = !!communityStateValue.mySnippets.find(
+    (item) => item.communityId === communityData.id
+  );
+
   return (
     <Flex direction="column" width="100%" height="146px">
       <Box height="50%" bg="blue.400" />
@@ -41,7 +48,8 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
               height="30px"
               pr={6}
               pl={6}
-              onClick={() => {}}
+              isLoading={loading}
+              onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}
             >
               {isJoined ? "Joined" : "Join"}
             </Button>
