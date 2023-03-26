5cd73235adc3051b700976694459e771deddc675
Ashrith Yerrapragada <ashrith.maudgalya@gmail.com>
2023-03-26 14:38:55 +0100

    page content layout

--- src/components/Layout/PageContent.tsx
+++ src/components/Layout/PageContent.tsx
@@ -6,13 +6,32 @@ type PageContentProps = {};
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
   console.log("children: ", children);
   return (
-    <Flex>
-      <Flex>
+    <Flex border="1px solid red" justify="center" p="16px 0px">
+      <Flex
+        border="1px solid green"
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+      >
         {/* LHS */}
-        <Flex>{children && children[0 as keyof typeof children]}</Flex>
+        <Flex
+          border="1px solid blue"
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+        >
+          {children && children[0 as keyof typeof children]}
+        </Flex>
 
         {/* RHS */}
-        <Flex>{children && children[1 as keyof typeof children]}</Flex>
+        <Flex
+          border="1px solid orange"
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+        >
+          {children && children[1 as keyof typeof children]}
+        </Flex>
       </Flex>
     </Flex>
   );
